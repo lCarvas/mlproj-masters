b85908233a06a7779a73e12fe2569545ae6a6611
Diogo Carvalho <76731842+lCarvas@users.noreply.github.com>
2025-10-18 17:07:06 +0100

    Config updates (#3)
    * chore(ruff): update configuration
    
    * chore: update vscode workspace config
    
    * chore(deps): remove nbqa
    
    * fix: ruff compliance

--- src/funcs/data_import.py
+++ src/funcs/data_import.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import polars as pl
 import seaborn as sns
+from IPython.display import display
 
 
 def import_data(file_path: str) -> pl.DataFrame:
@@ -24,30 +25,35 @@ def column_types(df: pl.DataFrame) -> dict[str, str]:
     Returns:
         dict[str, str]: A dictionary mapping column names to their data types.
     """
-    return {col: str(dtype) for col, dtype in zip(df.columns, df.dtypes)}
+    return {
+        col: str(dtype)
+        for col, dtype in zip(df.columns, df.dtypes, strict=True)
+    }
 
 
 def describe_data(
-    df: pl.DataFrame, metricFeatures: list[str], categoricalFeatures: list[str]
+    df: pl.DataFrame,
+    metric_features: list[str],
+    categorical_features: list[str],
 ) -> None:
     """Prints the number of duplicated rows and the count and percentage of missing values for each column in the DataFrame.
 
     Args:
         df (pl.DataFrame): The Polars DataFrame to analyze.
-        metricFeatures (list[str]): List of names of metric features.
-        categoricalFeatures (list[str]): List of names of categorical features.
+        metric_features (list[str]): List of names of metric features.
+        categorical_features (list[str]): List of names of categorical features.
     """
-    print(f"Duplicated: {df.is_duplicated().sum()}")
+    display(f"Duplicated: {df.is_duplicated().sum()}")
     df_len: int = df.shape[0]
 
-    print("Missing: ")
+    display("Missing: ")
     for col in df.columns:
         null_count = df.get_column(col).null_count()
-        print(f"{col}: {null_count}/{df_len} ({null_count / df_len:.2%})")
+        display(f"{col}: {null_count}/{df_len} ({null_count / df_len:.2%})")
 
-    for col in categoricalFeatures:
-        print(df.get_column(col).value_counts())
+    for col in categorical_features:
+        display(df.get_column(col).value_counts())
 
-    for i, col in enumerate(metricFeatures):
+    for i, col in enumerate(metric_features):
         plt.figure(i)
         sns.boxplot(x=col, data=df)
