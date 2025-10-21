92790177747d3504f441de45d9aa48b01aa21d19
Carvas <76731842+lCarvas@users.noreply.github.com>
2025-10-21 23:48:01 +0100

    feat: add get_correlation function

--- src/funcs/feature_selection.py
+++ src/funcs/feature_selection.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import polars as pl
 from IPython.display import display
 
@@ -13,3 +14,16 @@ def check_variance(df: pl.DataFrame) -> None:
         if current_column.dtype == pl.String:
             continue
         display(f"{col}: {current_column.var()}")
+
+
+def get_correlation(df: pl.DataFrame) -> pd.DataFrame:
+    """Calculates the correlation matrix of the DataFrame.
+
+    Args:
+        df (pl.DataFrame): The Polars DataFrame to analyze.
+
+    Returns:
+        pl.DataFrame: A Polars DataFrame representing the correlation matrix.
+    """
+    return df.to_pandas().corr()
+
