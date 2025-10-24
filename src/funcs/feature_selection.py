44b96bc17e2554e2351ef7aad72457f5fe2cf281
Diogo Carvalho <76731842+lCarvas@users.noreply.github.com>
2025-10-24 18:18:53 +0100

    Feature selection (#6)
    * feat: add get_correlation function
    
    * feat: add corr_heatmap function
    
    * feat: add ref function
    
    * feat: add lasso function
    
    * fix: fix get_correlation not using only metric features
    
    * refactor: change func arg types to duck types

--- src/funcs/feature_selection.py
+++ src/funcs/feature_selection.py
@@ -1,5 +1,17 @@
+from collections.abc import Sequence
+from typing import TYPE_CHECKING, Any
+
+import matplotlib.pyplot as plt
+import pandas as pd
 import polars as pl
+import seaborn as sns
 from IPython.display import display
+from sklearn.feature_selection import RFE
+from sklearn.linear_model import LassoCV, LinearRegression
+
+if TYPE_CHECKING:
+    from numpy import dtype, ndarray
+    from scipy.sparse._csr import csr_matrix
 
 
 def check_variance(df: pl.DataFrame) -> None:
@@ -13,3 +25,100 @@ def check_variance(df: pl.DataFrame) -> None:
         if current_column.dtype == pl.String:
             continue
         display(f"{col}: {current_column.var()}")
+
+
+def get_correlation(
+    df: pl.DataFrame, metric_features: Sequence[str]
+) -> pd.DataFrame:
+    """Calculates the correlation matrix of the DataFrame.
+
+    Args:
+        df (pl.DataFrame): The Polars DataFrame to analyze.
+        metric_features (Sequence[str]): List of numeric feature names.
+
+    Returns:
+        pl.DataFrame: A Polars DataFrame representing the correlation matrix.
+    """
+    return df.select(metric_features).to_pandas().corr()
+
+
+def corr_heatmap(corr: pd.DataFrame) -> None:
+    """Plots a heatmap of the correlation matrix.
+
+    Args:
+        corr (pd.DataFrame): The correlation matrix as a Pandas DataFrame.
+    """
+    plt.figure(figsize=(12, 10))
+    sns.heatmap(data=corr, annot=True, cmap=plt.cm.get_cmap("Reds"), fmt=".1")
+    plt.show()
+
+
+def rfe(
+    x_train: pl.DataFrame,
+    x_val: pl.DataFrame,
+    y_train: pl.Series,
+    y_val: pl.Series,
+    model: LinearRegression,
+) -> None:
+    """Performs Recursive Feature Elimination (RFE) to select the optimal number of features.
+
+    Args:
+        x_train (pl.DataFrame): Train DataFrame containing regressor features.
+        x_val (pl.DataFrame): Validation DataFrame containing regressor features.
+        y_train (pl.Series): Train Series containing target variable.
+        y_val (pl.Series): Validation Series containing target variable.
+        model (LinearRegression): The model to use for RFE.
+    """
+    high_score: float = 0
+    features_to_select: pd.Series = pd.Series()
+    nof: int = 0
+
+    for n in range(len(x_train.columns)):
+        rfe = RFE(estimator=model, n_features_to_select=n + 1)
+
+        x_train_rfe: ndarray[tuple[Any, ...], dtype[Any]] = rfe.fit_transform(
+            x_train.to_pandas(), y_train.to_pandas()
+        )
+        x_val_rfe: csr_matrix | ndarray[tuple[Any, ...], dtype[Any]] = (
+            rfe.transform(x_val.to_pandas())
+        )
+
+        y_train_pandas: pd.Series[int] = y_train.to_pandas()
+
+        model.fit(x_train_rfe, y_train_pandas)
+
+        val_score: float = float(model.score(x_val_rfe, y_val.to_pandas()))
+
+        if val_score >= high_score:
+            high_score = val_score
+            nof = n + 1
+
+            features_to_select = pd.Series(rfe.support_, index=x_train.columns)
+
+    display(f"Optimal number of features: {nof}")
+    display(f"Score with {nof} features: {high_score}")
+    display(f"Features to select:\n{features_to_select}")
+
+
+def lasso(
+    x_train: pl.DataFrame,
+    y_train: pl.Series,
+) -> pl.DataFrame:
+    """Performs Lasso regression to identify important features.
+
+    Args:
+        x_train (pl.DataFrame): Train DataFrame containing regressor features.
+        y_train (pl.Series): Train Series containing target variable.
+
+    Returns:
+        pl.DataFrame: A Polars DataFrame with features and their corresponding coefficients.
+    """
+    reg = LassoCV()
+    reg.fit(x_train.to_pandas(), y_train.to_pandas())
+
+    return pl.DataFrame(
+        {
+            "Feature": x_train.columns,
+            "Coefficient": reg.coef_,
+        }
+    ).sort("Coefficient")
